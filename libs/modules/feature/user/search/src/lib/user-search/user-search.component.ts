291df0b079425170fded696225ba9bea8ab53e46
victoralvesprof <victoralves.prof@gmail.com>
2024-03-09 14:17:14 -0300

    refactor: load user table

--- libs/modules/feature/user/search/src/lib/user-search/user-search.component.ts
+++ libs/modules/feature/user/search/src/lib/user-search/user-search.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatTableModule} from '@angular/material/table';
 import { UserSearchService } from 'user-data-access';
+import { DATATABLE, DATATABLEKEY } from '../constant/user-list.const';
 
 @Component({
   selector: 'lib-user-search',
@@ -11,7 +12,9 @@ import { UserSearchService } from 'user-data-access';
   styleUrl: './user-search.component.scss',
 })
 export class UserSearchComponent {
-  displayedColumns: string[] = ['id', 'avatar', 'name', 'email', 'biography'];
+  displayedColumns = DATATABLE;
+  displayedColumnKeys = DATATABLEKEY;
+
   users$ = this.userSearchService.getUsers();
 
   constructor(private userSearchService: UserSearchService) {}
