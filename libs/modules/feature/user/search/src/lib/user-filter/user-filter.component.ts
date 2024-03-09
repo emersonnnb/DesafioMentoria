e3eb18d14cbad7996015a44db73ad7af72e27607
victoralvesprof <victoralves.prof@gmail.com>
2024-03-09 19:17:58 -0300

    feat: filter to user

---
+++ libs/modules/feature/user/search/src/lib/user-filter/user-filter.component.ts
@@ -0,0 +1,39 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { User } from 'libs/modules/data-access/user/src/lib/models/user.model';
+import { debounceTime, distinctUntilChanged } from 'rxjs';
+import { MatTableDataSource } from '@angular/material/table';
+
+@Component({
+  selector: 'lib-user-filter',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatAutocompleteModule,
+  ],
+  templateUrl: './user-filter.component.html',
+  styleUrl: './user-filter.component.scss',
+})
+export class UserFilterComponent implements OnInit {
+  @Input() dataSource!: MatTableDataSource<User>;
+  @Output() filterChange = new EventEmitter<string>();
+
+  control = new FormControl('', { nonNullable: true });
+
+  ngOnInit(): void {
+    this.control.valueChanges
+      .pipe(debounceTime(400), distinctUntilChanged())
+      .subscribe((filterValue) => {
+        this.dataSource.filter = filterValue.trim().toLowerCase();
+        this.filterChange.emit(filterValue);
+      });
+  }
+}
--- libs/modules/feature/user/search/src/lib/user-search/user-search.component.ts
+++ libs/modules/feature/user/search/src/lib/user-search/user-search.component.ts
@@ -1,21 +1,59 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatTableModule} from '@angular/material/table';
+
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSort } from '@angular/material/sort';
+
 import { UserSearchService } from 'user-data-access';
 import { DATATABLE, DATATABLEKEY } from '../constant/user-list.const';
+import { User } from 'libs/modules/data-access/user/src/lib/models/user.model';
+import { UserFilterComponent } from '../user-filter/user-filter.component';
 
 @Component({
   selector: 'lib-user-search',
   standalone: true,
-  imports: [CommonModule, MatTableModule],
+  imports: [
+    CommonModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatInputModule,
+    MatFormFieldModule,
+    UserFilterComponent,
+  ],
   templateUrl: './user-search.component.html',
   styleUrl: './user-search.component.scss',
 })
-export class UserSearchComponent {
+export class UserSearchComponent implements OnInit, AfterViewInit {
   displayedColumns = DATATABLE;
   displayedColumnKeys = DATATABLEKEY;
+  dataSource = new MatTableDataSource<User>();
 
-  users$ = this.userSearchService.getUsers();
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  public input!: string;
 
   constructor(private userSearchService: UserSearchService) {}
+
+  ngOnInit(): void {
+    this.userSearchService.getUsers().subscribe((res) => {
+      this.dataSource.data = res;
+    });
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  onFilterChange(event: string) {
+    this.input = event;
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
