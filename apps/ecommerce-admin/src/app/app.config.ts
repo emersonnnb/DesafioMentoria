14d89d48987193973eccf784181098d6315a57ff
Emerson <emerson.barbosa@extreme.digital>
2024-03-10 14:03:57 -0300

    fix/adjust call angular/material anda import BrowserAnimationsModule

--- apps/ecommerce-admin/src/app/app.config.ts
+++ apps/ecommerce-admin/src/app/app.config.ts
@@ -1,8 +1,9 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes), provideHttpClient()],
+  providers: [provideRouter(appRoutes), provideHttpClient(), importProvidersFrom([BrowserAnimationsModule])],
 };
